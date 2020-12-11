a919151fe945077b8a58aaa530d43b5bb7ace9d0
tomoheko <dekirumadeyamenai@gmail.com>
2020-12-11 11:43:59 +0900

    カード情報詳細ページ

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root "top#index"
   resources :list, only: [:new, :create, :edit, :update, :destroy] do
-    resources :card, only: [:new, :create]
+    resources :card, only: [:new, :create, :show]
   end
 end
