47ec0d5e27abec0b964619caadce93ced7169024
tomoheko <dekirumadeyamenai@gmail.com>
2020-12-11 15:40:55 +0900

    削除機能

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root "top#index"
   resources :list, only: [:new, :create, :edit, :update, :destroy] do
-    resources :card, only: [:new, :create, :show, :edit, :update]
+    resources :card, except: [:index]
   end
 end
